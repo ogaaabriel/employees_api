345caa08a4836b513762c6ded62e180f3bd61b99
Gabriel Rodrigues <ogaaabriel@gmail.com>
2023-04-26 13:36:09 -0300

    Defined employee repository.

--- src/main/java/project/employee_manager/EmployeeManagerApplication.java
+++ src/main/java/project/employee_manager/EmployeeManagerApplication.java
@@ -1,13 +1,32 @@
 package project.employee_manager;
 
+import java.time.LocalDate;
+import java.time.Month;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import project.employee_manager.Employee.Employee;
+import project.employee_manager.Employee.EmployeeRepository;
+
 @SpringBootApplication
-public class EmployeeManagerApplication {
+public class EmployeeManagerApplication implements CommandLineRunner {
+
+	@Autowired
+	EmployeeRepository employeeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagerApplication.class, args);
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		employeeRepository.save(
+				new Employee("John", "Doe", "Gerente", "john.doe@email.com", LocalDate.of(2000, Month.JANUARY, 5)));
+	
 	}
 
 }
